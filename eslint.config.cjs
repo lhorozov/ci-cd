d6b6c5cdb0095a3b2b5f4d2042eba9ba905d0d8e
Lyuben Horozov <lhorozov@yahoo.com>
2026-02-15 22:28:30 +0200

    chore: migrate eslint config to v9 flat format

---
+++ eslint.config.cjs
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+        describe: "readonly",
+        it: "readonly",
+        expect: "readonly",
+        beforeAll: "readonly",
+        afterAll: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error"
+    }
+  }
+];
